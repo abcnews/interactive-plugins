1fdd513923b5caf024fd81666c8ada1f61e79262
Ash Kyd <kyd.ashley@abc.net.au>
2024-07-12 11:30:20 +1000

    feature: redesign to be svelter

--- src/components/NotificationContainer/NotificationContainer.svelte
+++ src/components/NotificationContainer/NotificationContainer.svelte
@@ -6,12 +6,13 @@
 
 <div class="notification-container__wrapper">
   <aside class="notification-container">
-    <div class="notification-container__icon"><slot name="icon"></slot></div>
-
     <div class="notification-container__text"><slot name="text"></slot></div>
 
     <label class="notification-container__form">
-      <div class="notification-container__label"><slot name="label"></slot></div>
+      <div class="notification-container__label">
+        <div class="notification-container__icon"><slot name="icon"></slot></div>
+        <span><slot name="label"></slot></span>
+      </div>
 
       <div class="notification-container__toggle">
         <ToggleButtonOnOff bind:value />
@@ -23,8 +24,10 @@
 <style>
   .notification-container__wrapper {
     container-type: inline-size;
+    text-align: center;
   }
   .notification-container {
+<<<<<<< Updated upstream
     --background: rgba(117, 128, 138, 0.15);
     --surface-utility-bolder: rgba(249, 250, 250, 0.81);
     --colour: black;
@@ -38,70 +41,80 @@
     justify-content: center;
     align-items: center;
     text-align: left;
-    max-width: 22.375rem;
-    margin: 0 auto;
-  }
-  .notification-container__icon :global(svg) {
-    width: 40px;
-    height: 40px;
-    margin: 0.69rem 1rem;
-  }
+=======
+    --Text-text-primary: black;
+    --surface-utility: rgba(117, 128, 138, 0.15);
 
-  .notification-container__text {
-    font-family: ABCSerif, serif;
-    font-size: 1.125rem;
-    font-style: normal;
-    font-weight: 600;
-    line-height: 125%;
-    flex: 1;
-  }
-  .notification-container__form {
-    display: flex;
+    border-radius: 0.75rem;
+    background: var(--surface-utility);
+    display: inline-flex;
+>>>>>>> Stashed changes
+    max-width: 22.375rem;
+    padding: 1rem;
+    flex-direction: column;
     justify-content: center;
-    width: 100%;
-    align-items: center;
-    padding: 0.8em;
-    border-radius: 1000000px;
-    background: var(--surface-utility-bolder);
-    margin-top: 1rem;
-  }
-  .notification-container__label {
-    /* Text Bold/md */
+    align-items: flex-start;
+    gap: 0.75rem;
+    color: var(--Text-text-primary);
+    text-align: center;
     font-family: 'ABC Sans Nova';
     font-size: 1rem;
     font-style: normal;
     font-weight: 700;
-    line-height: 150%; /* 1.5rem */
-    margin-right: 1.25rem;
-  }
-  @container (min-width: 651px) {
-    .notification-container {
-      max-width: inherit;
-      width: 100%;
-      border-radius: 1000000px;
-    }
-    .notification-container__icon {
-      margin-right: 0.5rem;
-    }
-    .notification-container__text {
-      margin-right: 1.25rem;
-      font-size: 1.25rem;
-    }
-
-    .notification-container__form {
-      width: auto;
-      margin: 0 0.5rem 0 0;
-    }
+    line-height: 125%; /* 1.25rem */
   }
 
   :global(.is-dark-mode) .notification-container {
+<<<<<<< Updated upstream
     --background: rgba(48, 50, 54, 0.6);
     --surface-utility-bolder: rgba(24, 25, 27, 0.75);
     --colour: #ebebeb;
     --icon-colour: white;
+=======
+    --Text-text-primary: white;
+    --surface-utility: rgba(48, 50, 54, 0.6);
+>>>>>>> Stashed changes
   }
-  :global(.is-dark-mode) .notification-container .notification-container__icon :global(svg path) {
-    stroke: white;
+
+  .notification-container__form {
+    display: flex;
+    height: 2.5rem;
+    padding: 1rem;
+    justify-content: center;
+    align-items: center;
+    gap: 1.5rem;
+    align-self: stretch;
+    border-radius: 1000rem;
+    background: var(--surface-utility, rgba(117, 128, 138, 0.15));
+    cursor: pointer;
+  }
+
+  .notification-container__label {
+    display: flex;
+    gap: 0.5rem;
+    align-items: center;
+  }
+
+  .notification-container__icon {
+    display: flex;
+  }
+
+  .notification-container__icon :global(svg) {
+    width: 20px;
+    height: 20px;
+    stroke: var(--Text-text-primary);
+  }
+
+  @container (min-width: 651px) {
+    .notification-container {
+      flex-direction: row;
+      align-items: center;
+      max-width: unset;
+      margin: 0 auto;
+      border-radius: 1000rem;
+      padding: 0.75rem 0.75rem 0.75rem 1rem;
+      gap: 2rem;
+    }
   }
 
   :global(#audio-visual-plugin-mount, #reduced-motion-plugin-mount) {
